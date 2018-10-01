4629511f473d9421e932ce8b0bdd1079e16846bf
jbk-at-555341550874 <jbk@wemakeprice.com>
2018-10-01 18:38:27 +0900

    spring-boot-vuejs controller annotation add

--- spring-boot-vuejs/src/main/java/com/sample/web/controller/RouteController.java
+++ spring-boot-vuejs/src/main/java/com/sample/web/controller/RouteController.java
@@ -1,7 +1,9 @@
 package com.sample.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class RouteController {
 
     @RequestMapping("/")
