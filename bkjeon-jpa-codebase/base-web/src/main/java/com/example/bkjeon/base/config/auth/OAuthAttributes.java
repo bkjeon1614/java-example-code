139df2dd8bcfda7b62158963e508eef77d729424
bongkeun-jeon <gcijdfdo@gmail.com>
2020-10-20 00:34:33 +0900

    네이버 oauth 로그인 연동 추가

--- bkjeon-jpa-codebase/base-web/src/main/java/com/example/bkjeon/base/config/auth/OAuthAttributes.java
+++ bkjeon-jpa-codebase/base-web/src/main/java/com/example/bkjeon/base/config/auth/OAuthAttributes.java
@@ -36,6 +36,10 @@ public class OAuthAttributes {
         String userNameAttributeName,
         Map<String, Object> attributes
     ) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -48,6 +52,21 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(
+        String userNameAttributeName,
+        Map<String, Object> attributes
+    ) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
