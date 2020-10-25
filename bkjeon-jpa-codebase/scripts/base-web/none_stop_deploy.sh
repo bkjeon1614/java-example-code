72aefcb1ca01710d0adc4eb83da144f362a5f822
bongkeun-jeon <gcijdfdo@gmail.com>
2020-10-25 19:57:47 +0900

    스크립트 권한 관련 추가

--- bkjeon-jpa-codebase/scripts/base-web/none_stop_deploy.sh
+++ bkjeon-jpa-codebase/scripts/base-web/none_stop_deploy.sh
@@ -3,4 +3,7 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 
+chmod +x ${ABSDIR}/stop.sh
+chmod +x ${ABSDIR}/start.sh
+chmod +x ${ABSDIR}/health.sh
 ${ABSDIR}/stop.sh && ${ABSDIR}/start.sh && ${ABSDIR}/health.sh
