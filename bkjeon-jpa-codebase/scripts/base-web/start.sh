3d882114b06454552f1080db3b18e9951c28214d
bongkeun-jeon <gcijdfdo@gmail.com>
2020-10-25 13:51:11 +0900

    start script 변경

--- bkjeon-jpa-codebase/scripts/base-web/start.sh
+++ bkjeon-jpa-codebase/scripts/base-web/start.sh
@@ -9,7 +9,7 @@ PROJECT_NAME=base-web
 
 echo "> Build File Copy $REPOSITORY"
 echo "> cp $REPOSITORY/${PROJECT_NAME}*.jar $REPOSITORY/"
-cp $REPOSITORY/${PROJECT_NAME}*.jar $REPOSITORY/
+cp $REPOSITORY/${PROJECT_NAME}*.jar $REPOSITORY/deploy/
 
 echo "> New Application Deploy"
 JAR_NAME=$(ls -tr $REPOSITORY/${PROJECT_NAME}*.jar | tail -n 1)
@@ -22,4 +22,4 @@ echo "> $JAR_NAME Start"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME => profile=$IDLE_PROFILE Start"
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/deploy/nohup.out 2>&1 &
