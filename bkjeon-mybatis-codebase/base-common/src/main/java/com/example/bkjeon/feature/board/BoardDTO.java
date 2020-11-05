8420e9fe5acf134ec365d87ef708695e838df975
bongkeun-jeon <gcijdfdo@gmail.com>
2020-11-05 20:42:34 +0900

    테스트 케이스 추가

--- bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/feature/board/BoardDTO.java
+++ bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/feature/board/BoardDTO.java
@@ -1,14 +1,14 @@
 package com.example.bkjeon.feature.board;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 
+@ToString
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class BoardDTO {
 
     @NotEmpty(message = "제목을 입력하여 주시길 바랍니다.")
