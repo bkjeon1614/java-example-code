390334f3f68fd5a55b60ae9cd965f0d2b2a184b0
bongkeun-jeon <gcijdfdo@gmail.com>
2021-03-16 18:21:58 +0900

    eslint test2

--- spring-boot-react-typescript/frontend/src/App.tsx
+++ spring-boot-react-typescript/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+function App(sd) {
   return (
     <div className="App">
       <header className="App-header">
