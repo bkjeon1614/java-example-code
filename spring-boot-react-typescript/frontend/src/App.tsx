fd0dd1c14cc749c014f46a7245bac84b831e9bdd
bongkeun-jeon <gcijdfdo@gmail.com>
2021-03-16 18:25:03 +0900

    TES

--- spring-boot-react-typescript/frontend/src/App.tsx
+++ spring-boot-react-typescript/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App(sd) {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
