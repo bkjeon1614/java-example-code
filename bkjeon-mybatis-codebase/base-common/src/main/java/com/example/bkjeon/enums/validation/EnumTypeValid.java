47e804708abd5a8a92eb9f5fcbf4b888eaa9368d
bongkeun-jeon <gcijdfdo@gmail.com>
2021-05-13 20:30:43 +0900

    설명추가

--- bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/enums/validation/EnumTypeValid.java
+++ bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/enums/validation/EnumTypeValid.java
@@ -11,6 +11,11 @@ import java.lang.annotation.*;
 public @interface EnumTypeValid {
 
     String message() default "invalid parameter!!";
+
+    /**
+     * target 속성으로 EnumType.class 를 지정해주면 실제 bean validator에 의해 ConstraintValidator를 구현한 클래스들을 실행하고
+     * intialize(), isValid() 의 실행순서를 가지면서 실행된다.
+     */
     Class<? extends EnumTypeValueInterface> target();
 
     // 하기 groups, payload 를 추가하지 않으면 javax.validation.ConstraintDefinitionException: HV000074 오류 발생
--- bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/enums/validation/EnumValidator.java
+++ bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/enums/validation/EnumValidator.java
@@ -10,6 +10,11 @@ public class EnumValidator implements ConstraintValidator<EnumTypeValid, String>
 
     private List<String> types;
 
+    /**
+     * 파라미터로 실제 클래스에 사용된 @EnumTypeValid Annotation이 매핑되어 target에는 EnumType.class 타입이
+     * 존재하게 된다. 해당 타입을 얻었으니 전체 enum 상수 constants 들을 얻을 수 있고
+     * 이를 통해서 모든 타입들을 얻을 수 있다.
+     */
     @Override
     public void initialize(EnumTypeValid constraintAnnotation) {
         types = Arrays.stream(constraintAnnotation.target().getEnumConstants())
@@ -17,6 +22,10 @@ public class EnumValidator implements ConstraintValidator<EnumTypeValid, String>
             .collect(Collectors.toList());
     }
 
+    /**
+     * 실제 유효성 판단을 내리는 곳으로 initialize를 통해서 타입들을 얻었으니 contains 메소드를 활용해서
+     * 현재 파라미터 클래스의 enum 타입 변수로 들어온 값이 types에 존재하는지만 판단하면 된다.
+     */
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         return types.contains(value);
