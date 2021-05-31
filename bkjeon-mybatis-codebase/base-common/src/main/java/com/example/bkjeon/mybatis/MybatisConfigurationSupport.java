ffb8493299bec68f5f37eb3c9574218e5cd4d092
bongkeun-jeon <bongkeun-jeon@cj.net>
2021-05-31 16:19:32 +0900

    multi connection add

---
+++ bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/mybatis/MybatisConfigurationSupport.java
@@ -0,0 +1,77 @@
+package com.example.bkjeon.mybatis;
+
+import javax.sql.DataSource;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+@Getter
+public class MybatisConfigurationSupport {
+
+    private final MybatisProperties mybatisProperties;
+    private final Interceptor[] interceptors;
+    private final ResourceLoader resourceLoader;
+    private final DatabaseIdProvider databaseIdProvider;
+
+    public MybatisConfigurationSupport(
+        MybatisProperties mybatisProperties,
+        ObjectProvider<Interceptor[]> interceptorsProvider,
+        ResourceLoader resourceLoader,
+        ObjectProvider<DatabaseIdProvider> databaseIdProvider) {
+        this.mybatisProperties = mybatisProperties;
+        this.interceptors = interceptorsProvider.getIfAvailable();
+        this.resourceLoader = resourceLoader;
+        this.databaseIdProvider = databaseIdProvider.getIfAvailable();
+        log.info("=============================== mybatis ===============================");
+    }
+
+    public SqlSessionFactory build(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setVfs(SpringBootVFS.class);
+        if (StringUtils.hasText(mybatisProperties.getConfigLocation())) {
+            sqlSessionFactoryBean
+                .setConfigLocation(
+                    resourceLoader.getResource(mybatisProperties.getConfigLocation()));
+        }
+        if (mybatisProperties.getConfiguration() != null) {
+            Configuration configuration = new Configuration();
+            BeanUtils.copyProperties(mybatisProperties.getConfiguration(), configuration);
+            sqlSessionFactoryBean.setConfiguration(configuration);
+        }
+        if (mybatisProperties.getConfigurationProperties() != null) {
+            sqlSessionFactoryBean
+                .setConfigurationProperties(mybatisProperties.getConfigurationProperties());
+        }
+        if (!ObjectUtils.isEmpty(interceptors)) {
+            sqlSessionFactoryBean.setPlugins(interceptors);
+        }
+        if (databaseIdProvider != null) {
+            sqlSessionFactoryBean.setDatabaseIdProvider(databaseIdProvider);
+        }
+        if (StringUtils.hasLength(mybatisProperties.getTypeAliasesPackage())) {
+            sqlSessionFactoryBean.setTypeAliasesPackage(mybatisProperties.getTypeAliasesPackage());
+        }
+        if (StringUtils.hasLength(mybatisProperties.getTypeHandlersPackage())) {
+            sqlSessionFactoryBean
+                .setTypeHandlersPackage(mybatisProperties.getTypeHandlersPackage());
+        }
+        if (!ObjectUtils.isEmpty(mybatisProperties.resolveMapperLocations())) {
+            sqlSessionFactoryBean.setMapperLocations(mybatisProperties.resolveMapperLocations());
+        }
+        return sqlSessionFactoryBean.getObject();
+    }
+
+}
---
+++ bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/mybatis/OracleDataSourceConfig.java
@@ -0,0 +1,43 @@
+package com.example.bkjeon.mybatis;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@MapperScan(
+    basePackages = "com.example.bkjeon.mapper",
+    annotationClass = OracleConnMapper.class,
+    sqlSessionFactoryRef = "oracleSessionFactory")
+public class OracleDataSourceConfig {
+
+  @Autowired
+  private MybatisOracleConfigurationSupport myBatisConfigurationSupport;
+
+  @Primary
+  @Bean(name = "oracleDataSource")
+  @ConfigurationProperties(prefix = "spring.oracle.datasource")
+  public DataSource oracleDataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Primary
+  @Bean(name = "oracleSessionFactory")
+  public SqlSessionFactory oracleSessionFactory(
+      @Qualifier("oracleDataSource") DataSource dataSource) throws Exception {
+    return myBatisConfigurationSupport.build(dataSource);
+  }
+
+  @Bean(name="oracleSession")
+  public SqlSessionTemplate mySqlSession(@Qualifier("oracleSessionFactory") SqlSessionFactory oracleSessionFactory) throws Exception {
+    return new SqlSessionTemplate(oracleSessionFactory);
+  }
+}
