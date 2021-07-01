1410398a36e9deea552ba682b4ab79d1361dd454
bongkeun-jeon <gcijdfdo@gmail.com>
2021-07-01 22:09:34 +0900

    test code add

--- bkjeon-jpa-codebase/base-api/src/main/resources/sql/data-h2.sql
+++ bkjeon-jpa-codebase/base-api/src/main/resources/sql/data-h2.sql
@@ -1,8 +1,10 @@
-/*insert into posts (title, author, content) values ('테스트1', 'test1@gmail.com', '테스트1의 본문');
-insert into posts (title, author, content) values ('테스트2', 'test2@gmail.com', '테스트2의 본문');*/
+insert into posts (title, author, content) values ('테스트1', 'test1@gmail.com', '테스트1의 본문');
+insert into posts (title, author, content) values ('테스트2', 'test2@gmail.com', '테스트2의 본문');
+insert into posts (title, author, content) values ('테스트3', 'test3@gmail.com', '테스트3의 본문');
 
 INSERT INTO USER (USER_ID, USERNAME, PASSWORD, NICKNAME, ACTIVATED) VALUES (1, 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
 INSERT INTO USER (USER_ID, USERNAME, PASSWORD, NICKNAME, ACTIVATED) VALUES (2, 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
+INSERT INTO USER (USER_ID, USERNAME, PASSWORD, NICKNAME, ACTIVATED) VALUES (3, 'bkjeon', '$2a$10$5LObc9rbmDCnzIO70RqGbe4lE1Ye1Dc.58GUIRTmcTijul2WCdF.e', 'bkjeon', 1);
 
 INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_USER');
 INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_ADMIN');
@@ -10,6 +12,8 @@ INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_ADMIN');
 INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_USER');
 INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
 INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (2, 'ROLE_USER');
+INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (3, 'ROLE_USER');
+INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (3, 'ROLE_ADMIN');
 
 INSERT INTO board (title, contents, author) values ('테스트11111', '테스트2의 본문111111', 'bkjeon');
 INSERT INTO board (title, contents, author) values ('테스트2', '테스트2의 본문', 'bkjeon');
--- bkjeon-jpa-codebase/base-api/src/test/java/com/example/bkjeon/base/api/v1/example/PostControllerTest.java
+++ bkjeon-jpa-codebase/base-api/src/test/java/com/example/bkjeon/base/api/v1/example/PostControllerTest.java
@@ -1,15 +1,11 @@
-package com.example.bkjeon.base.api.post;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+package com.example.bkjeon.base.api.v1.example;
 
 import com.example.bkjeon.base.api.v1.controller.example.dto.PostsSaveRequestDTO;
 import com.example.bkjeon.base.api.v1.controller.example.dto.PostsUpdateRequestDTO;
+import com.example.bkjeon.base.api.v1.controller.security.jwt.dto.LoginDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +13,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Transactional
 public class PostControllerTest {
+
+    private String accessToken;
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,10 +33,27 @@ public class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void beforeEach() throws Exception {
+        LoginDTO loginDTO = LoginDTO.builder()
+            .username("bkjeon")
+            .password("wjsqhdrms")
+            .build();
+        String loginRequestJsonString = objectMapper.writeValueAsString(loginDTO);
+        MvcResult mvcResult = mockMvc.perform(post("/v1/security/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(loginRequestJsonString))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+        accessToken = JsonPath.read(mvcResult.getResponse().getContentAsString(), "$.token");
+    }
+
     @Test
     @DisplayName("[GET] v1/posts/1")
     void postFindById() throws Exception {
         mockMvc.perform(get("/v1/posts/1")
+            .header("ACCESS-TOKEN", accessToken)
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andDo(print());
@@ -49,6 +69,7 @@ public class PostControllerTest {
         String requestJsonString = objectMapper.writeValueAsString(postsSaveRequestDTO);
 
         mockMvc.perform(post("/v1/posts")
+            .header("ACCESS-TOKEN", accessToken)
             .contentType(MediaType.APPLICATION_JSON)
             .content(requestJsonString))
             .andExpect(status().isOk())
@@ -65,6 +86,7 @@ public class PostControllerTest {
         String requestJsonString = objectMapper.writeValueAsString(postsUpdateRequestDTO);
 
         mockMvc.perform(put("/v1/posts/1")
+            .header("ACCESS-TOKEN", accessToken)
             .contentType(MediaType.APPLICATION_JSON)
             .content(requestJsonString))
             .andExpect(status().isOk())
@@ -72,9 +94,10 @@ public class PostControllerTest {
     }
 
     @Test
-    @DisplayName("[DELETE] v1/posts/1")
+    @DisplayName("[DELETE] v1/posts/3")
     void postDelete() throws Exception {
-        mockMvc.perform(delete("/v1/posts/1")
+        mockMvc.perform(delete("/v1/posts/3")
+            .header("ACCESS-TOKEN", accessToken)
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andDo(print());
