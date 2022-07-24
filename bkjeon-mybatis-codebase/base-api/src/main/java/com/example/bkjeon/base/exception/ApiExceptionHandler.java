9156da9fa7ddf60b0866cbf8a6a2b4a214b9b2a0
[전시개발팀]전봉근 <bongkeun.jeon@lotte.net>
2022-07-24 11:55:10 +0900

    ExceptionHandler 추가

---
+++ bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/exception/ApiExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.bkjeon.base.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice(annotations = RestController.class)
+@Slf4j
+public class ApiExceptionHandler {
+
+
+
+}
