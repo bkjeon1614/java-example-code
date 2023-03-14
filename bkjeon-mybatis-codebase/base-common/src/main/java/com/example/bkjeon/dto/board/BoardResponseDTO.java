8957282ea879cd5c6d7a57bdf87873142251c155
[전시개발팀]전봉근 <bongkeun.jeon@lotte.net>
2023-03-14 16:10:40 +0900

    board swagger

--- bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/dto/board/BoardResponseDTO.java
+++ bkjeon-mybatis-codebase/base-common/src/main/java/com/example/bkjeon/dto/board/BoardResponseDTO.java
@@ -2,6 +2,8 @@ package com.example.bkjeon.dto.board;
 
 import com.example.bkjeon.entity.board.Board;
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -9,15 +11,28 @@ import java.time.LocalDateTime;
 @Getter
 public class BoardResponseDTO {
 
+    @Schema(description = "게시물 번호")
     private Long boardNo;
+
+    @Schema(description = "게시물 그룹 번호")
     private Long groupNo;
+
+    @Schema(description = "게시물 정렬 번호")
     private Integer sortSeq;
+
+    @Schema(description = "게시물 뎁스 번호")
     private Integer boardLvl;
+
+    @Schema(description = "게시물 제목")
     private String boardTitle;
+
+    @Schema(description = "게시물 내용")
     private String boardContents;
 
+    @Schema(description = "게시물 등록자")
     private String sysRegrId;
 
+    @Schema(description = "게시물 등록일자")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime sysRegDtime;
 
