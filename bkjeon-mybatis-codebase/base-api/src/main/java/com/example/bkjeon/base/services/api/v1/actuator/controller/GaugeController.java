ea2e918b0ea56f547ab9b1bb552f86858df10913
[전시개발팀]전봉근 <bongkeun.jeon@lotte.net>
2023-11-27 14:46:16 +0900

    gauge

---
+++ bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/services/api/v1/actuator/controller/GaugeController.java
@@ -0,0 +1,31 @@
+package com.example.bkjeon.base.services.api.v1.actuator.controller;
+
+import com.example.bkjeon.base.services.api.v1.actuator.service.gauge.QueueManager;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/gauges")
+@RequiredArgsConstructor
+public class GaugeController {
+
+    private static final String METRIC_NAME = "my.queue.size";
+
+    private final QueueManager queueManager;
+    private final MeterRegistry meterRegistry;
+
+    @GetMapping("req")
+    public double gaugeReq() {
+        Gauge.builder(METRIC_NAME, queueManager, QueueManager::increment).register(meterRegistry);
+        return Gauge.builder(METRIC_NAME, queueManager, QueueManager::getSize)
+            .register(meterRegistry)
+            .value();
+    }
+
+}
