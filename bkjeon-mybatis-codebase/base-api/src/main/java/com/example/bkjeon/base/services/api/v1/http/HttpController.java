cdedfae42e0070ace3314a45cedeb4b8977fbbe6
[전시개발팀]전봉근 <bongkeun.jeon@lotte.net>
2024-02-07 19:22:49 +0900

    http

--- bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/services/api/v1/http/HttpController.java
+++ bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/services/api/v1/http/HttpController.java
@@ -1,16 +1,16 @@
 package com.example.bkjeon.base.services.api.v1.http;
 
+import com.example.bkjeon.dto.actuator.LibarayInfo;
+import com.example.bkjeon.util.HttpUtil;
+import io.swagger.annotations.ApiOperation;
 import javax.servlet.http.HttpServletRequest;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.util.UrlPathHelper;
-
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -29,6 +29,16 @@ public class HttpController {
 			.queryParam("p1", "test1")
 			.queryParam("p2", "test2")
 			.build(true);
+		return uriComponents.toUriString();
+	}
+
+	@ApiOperation("URL Create2 (쿼리 스트링 포함)")
+	@GetMapping("isCreateUrl2")
+	public String getCreateUrl2() {
+		UriComponents uriComponents = UriComponentsBuilder
+			.fromUriString("https://test.bkjeon.com")
+			.queryParams(HttpUtil.dtoToMapConverter(LibarayInfo.builder().name("test").build()))
+			.build();
 		return uriComponents.toUriString();
 	}
 
