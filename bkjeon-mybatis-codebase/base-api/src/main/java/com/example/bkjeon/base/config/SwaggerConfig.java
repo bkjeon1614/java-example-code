9c60428b02a35d3426fa5c6df6f1a37e4961d6e7
전봉근(롯데쇼핑 e커머스-파인딩플랫폼개발팀) <gcijdfdo@gmail.com>
2024-08-21 18:56:33 +0900

    i

--- bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/config/SwaggerConfig.java
+++ bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.example.bkjeon.base.config;
 
+import com.example.bkjeon.enums.exception.ErrorCode;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
@@ -24,6 +25,9 @@ public class SwaggerConfig {
     @Bean
     public Docket adminApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)  // false 로 설정하면 Swagger 에서 제공해주는 응답코드(200, 401, 403, 404)에 대한 기본 메시지를 제거해준다.
+            .globalResponseMessage(RequestMethod.GET, getResponseMessageList())
+            .globalResponseMessage(RequestMethod.POST, getResponseMessageList())
             .groupName("v1")
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.example.bkjeon.base.services.api.v1"))
@@ -34,23 +38,41 @@ public class SwaggerConfig {
 
     @Bean
     public Docket internalApi() {
-        final List<ResponseMessage> globalResponses = Arrays.asList(
-            new ResponseMessageBuilder().code(200).message(HttpStatus.OK.getReasonPhrase()).build(),
-            new ResponseMessageBuilder().code(400).message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build(),
-            new ResponseMessageBuilder().code(401).message(HttpStatus.UNAUTHORIZED.getReasonPhrase()).build(),
-            new ResponseMessageBuilder().code(403).message(HttpStatus.FORBIDDEN.getReasonPhrase()).build(),
-            new ResponseMessageBuilder().code(500).message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()).build());
-
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)  // false 로 설정하면 Swagger 에서 제공해주는 응답코드(200, 401, 403, 404)에 대한 기본 메시지를 제거해준다.
-            .globalResponseMessage(RequestMethod.GET, globalResponses)
-            .globalResponseMessage(RequestMethod.POST, globalResponses)
+            .globalResponseMessage(RequestMethod.GET, getResponseMessageList())
+            .globalResponseMessage(RequestMethod.POST, getResponseMessageList())
             .groupName("v2")
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.example.bkjeon.base.services.api.v2"))
             .paths(PathSelectors.any())
             .build()
             .apiInfo(apiInfo("Code Base APIs V2", "Code Base APIs..", "v2"));
+    }
+
+    private List<ResponseMessage> getResponseMessageList() {
+        return Arrays.asList(
+            new ResponseMessageBuilder().code(200).message(HttpStatus.OK.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(400).message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(401).message(HttpStatus.UNAUTHORIZED.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(403).message(HttpStatus.FORBIDDEN.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.BAD_REQUEST.getStatus()).message(ErrorCode.BAD_REQUEST.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.METHOD_ARGUMENT_TYPE_ENUM_BINDING_MISMATCH.getStatus()).message(ErrorCode.METHOD_ARGUMENT_TYPE_ENUM_BINDING_MISMATCH.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.UNAUTHORIZED.getStatus()).message(ErrorCode.UNAUTHORIZED.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.FORBIDDEN.getStatus()).message(ErrorCode.FORBIDDEN.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.NOT_FOUND.getStatus()).message(ErrorCode.NOT_FOUND.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.METHOD_NOT_ALLOWED.getStatus()).message(ErrorCode.METHOD_NOT_ALLOWED.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.CONFLICT.getStatus()).message(ErrorCode.CONFLICT.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.PRECONDITION_FAILED.getStatus()).message(ErrorCode.PRECONDITION_FAILED.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.TOO_MANY_REQUESTS.getStatus()).message(ErrorCode.TOO_MANY_REQUESTS.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.INTERNAL_SERVER_ERROR.getStatus()).message(ErrorCode.INTERNAL_SERVER_ERROR.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.BAD_GATEWAY.getStatus()).message(ErrorCode.BAD_GATEWAY.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.SERVICE_UNAVAILABLE.getStatus()).message(ErrorCode.SERVICE_UNAVAILABLE.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.INVALID_INPUT_VALUE_BINDING_ERROR.getStatus()).message(ErrorCode.INVALID_INPUT_VALUE_BINDING_ERROR.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.BOARD_INSERT_FAILED.getStatus()).message(ErrorCode.BOARD_INSERT_FAILED.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.BOARD_UPDATE_FAILED.getStatus()).message(ErrorCode.BOARD_UPDATE_FAILED.getMessage()).build(),
+            new ResponseMessageBuilder().code(ErrorCode.BOARD_EMPTY.getStatus()).message(ErrorCode.BOARD_EMPTY.getMessage()).build()
+        );
     }
 
     private ApiInfo apiInfo(String title, String description, String version) {
