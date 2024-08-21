c3da38f688f42d84f9a77f1cd048e3bc57675845
전봉근(롯데쇼핑 e커머스-파인딩플랫폼개발팀) <gcijdfdo@gmail.com>
2024-08-21 18:25:35 +0900

    i

--- bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/config/SwaggerConfig.java
+++ bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/config/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package com.example.bkjeon.base.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,7 +34,17 @@ public class SwaggerConfig {
 
     @Bean
     public Docket internalApi() {
+        final List<ResponseMessage> globalResponses = Arrays.asList(
+            new ResponseMessageBuilder().code(200).message(HttpStatus.OK.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(400).message(HttpStatus.BAD_REQUEST.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(401).message(HttpStatus.UNAUTHORIZED.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(403).message(HttpStatus.FORBIDDEN.getReasonPhrase()).build(),
+            new ResponseMessageBuilder().code(500).message(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()).build());
+
         return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)  // false 로 설정하면 Swagger 에서 제공해주는 응답코드(200, 401, 403, 404)에 대한 기본 메시지를 제거해준다.
+            .globalResponseMessage(RequestMethod.GET, globalResponses)
+            .globalResponseMessage(RequestMethod.POST, globalResponses)
             .groupName("v2")
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.example.bkjeon.base.services.api.v2"))
