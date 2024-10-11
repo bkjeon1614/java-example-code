a2b387ef1e023f4e5546a40b1fb949935a729c1d
전봉근(롯데쇼핑 e커머스-파인딩플랫폼개발팀) <gcijdfdo@gmail.com>
2024-10-11 20:08:49 +0900

    aysnc 관련

---
+++ java11/bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/services/api/v1/async/AsyncController.java
@@ -0,0 +1,68 @@
+package com.example.bkjeon.base.services.api.v1.async;
+
+import io.swagger.annotations.ApiOperation;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/async")
+public class AsyncController {
+
+    private final AsyncService asyncService;
+
+    @ApiOperation("@Async 테스트")
+    @GetMapping("/annotation")
+    public void isAsyncAnnotation() throws InterruptedException, ExecutionException {
+        /**
+         * future.get() 은 블로킹을 통해 요청 결과가 올 때까지 기다리는 역할을 한다.
+         * 그러므로 비동기 블로킹 방식이 되어버려 성능이 좋지 않다. 보통 Future 는 사용하지 않는다.
+         * [결과]
+         * ============= Feature Task Start - 1
+         * ============= isAsyncAnnotationReturn : bkjeon-1
+         * ============= Feature Task Start - 2
+         * ============= isAsyncAnnotationReturn : bkjeon-2
+         * ============= Feature Task Start - 3
+         * ============= isAsyncAnnotationReturn : bkjeon-3
+         * ============= Feature Task Start - 4
+         * ============= isAsyncAnnotationReturn : bkjeon-4
+         * ============= Feature Task Start - 5
+         * ============= isAsyncAnnotationReturn : bkjeon-5
+         */
+        for (int i = 1; i <= 5; i++) {
+            Future<String> future = asyncService.isFeatureReturn(i + "");
+            log.info("============= isAsyncAnnotationReturn : {}", future.get());
+        }
+
+        log.info("=================================================================");
+
+        /**
+         * ListenableFuture 은 콜백을 통해 논블로킹 방식으로 작업을 처리할 수 있다.
+         * addCallback() 메소드의 첫 번째 파라미터는 작업 완료 콜백 메소드, 두 번째 파라미터는 작업 실패 콜백 메소드를 정의하면 된다.
+         * 스레드 풀의 core 스레드를 3개로 설정했으므로 "Task Start" 메시지가 처음에 3개 찍힘
+         * [결과]
+         * ============= Feature Task Start - 1
+         * ============= Feature Task Start - 2
+         * ============= Feature Task Start - 3
+         * bkjeon-2
+         * bkjeon-1
+         * bkjeon-3
+         * ============= Feature Task Start - 4
+         * ============= Feature Task Start - 5
+         * bkjeon-5
+         * bkjeon-4
+         */
+        for (int i = 1; i <= 5; i++) {
+            ListenableFuture<String> listenableFuture = asyncService.isListenableFutureReturn(i + "");
+            listenableFuture.addCallback(System.out::println, error -> System.out.println(error.getMessage()));
+        }
+    }
+
+}
---
+++ java11/bkjeon-mybatis-codebase/base-api/src/main/java/com/example/bkjeon/base/services/api/v1/async/AsyncService.java
@@ -0,0 +1,28 @@
+package com.example.bkjeon.base.services.api.v1.async;
+
+import java.util.concurrent.Future;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@Slf4j
+@Service
+public class AsyncService {
+
+    @Async("threadPoolTaskExecutor")
+    public Future<String> isFeatureReturn(String message) throws InterruptedException {
+        log.info("============= Feature Task Start - {}", message);
+        Thread.sleep(3000);
+        return new AsyncResult<>("bkjeon-" + message);
+    }
+
+    @Async("threadPoolTaskExecutor")
+    public ListenableFuture<String> isListenableFutureReturn(String message) throws InterruptedException {
+        log.info("============= ListenableFuture Task Start - {}", message);
+        Thread.sleep(3000);
+        return new AsyncResult<>("bkjeon-" + message);
+    }
+
+}
